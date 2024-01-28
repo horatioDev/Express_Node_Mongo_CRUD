7a4714656e073a49b8d0fde5d43454cdde534400
Ray-Xavier-2021 <horatioxhanley@gmail.com>
2024-01-28 18:14:23 -0500

    Update server.js and implement READ operation to serve index.html to browser

--- team_tracker/server.js
+++ team_tracker/server.js
@@ -9,5 +9,34 @@ app.listen(PORT, function() {
 });
 // ----------------------------------------------------------------------------
 
+// Create ---------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+
+// Read -----------------------------------------------------------------------
+/*
+We handle GET request w/ get method:
+app.get(endpoint, callback);
+
+domain_name: www.website.com/dir/file/
+endpoint: is anything after domain_name (/dir/file/)
+callback: tells the server what to do when the requested endpoint  matches the endpoint in the route.
+
+It takes (req, res) as parameters where req is the HTTP request and res is the  HTTP response.
+
+app.get('/', (req, res) => {handle req})
+*/
+app.get('/', (req, res) => {
+  // Let'serve index.html
+  // __dirname is the current directory you're in. 
+  res.sendFile(__dirname, + '/index.html')
+});
+// ----------------------------------------------------------------------------
+
+// Update ---------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+
+// Delete ---------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+
 // Test server
 console.log('Team Tracker:  Create, Read, Update & Delete');
