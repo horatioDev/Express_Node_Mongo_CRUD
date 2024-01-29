8a758ce35851bc9d87c4cfe8539a4ebd6a9889eb
Ray-Xavier-2021 <horatioxhanley@gmail.com>
2024-01-29 02:01:57 -0500

    Add form to html

--- server.js
+++ server.js
@@ -7,9 +7,36 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
   console.log(`Listening on localhost:${PORT}`)
 });
+
+// Run server
+// cd working_dir && node server.js
+// ----------------------------------------------------------------------------
+
+// Nodemon --------------------------------------------------------------------
+/*
+Nodemon restarts the server automatically when you save a file that’s used by the server.js. 
+npm install nodemon --save-dev
+
+Update script in package.json
+"scripts": {
+  "dev": "nodemon server.js"
+}
+
+npm run dev to trigger nodemon server.js
+*/
 // ----------------------------------------------------------------------------
 
 // Create ---------------------------------------------------------------------
+/*
+Browsers can only perform a Create operation if they send a POST request to the server though a <form> or javascript.
+
+To send a POST request the index.html needs a <form> element.
+The form should have an action, method attribute and a name attribute on each input element:
+action: endpoint
+method: Request
+name: Descriptive name
+*/
+
 // ----------------------------------------------------------------------------
 
 // Read -----------------------------------------------------------------------
@@ -25,6 +52,7 @@ It takes (req, res) as parameters where req is the HTTP request and res is the  
 
 app.get('/', (req, res) => {handle req})
 */
+
 app.get('/', (req, res) => {
   // Let'serve index.html
   // __dirname is the current directory you're in. 
