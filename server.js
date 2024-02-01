5430bfcd89aee8ee86a53c080d34a1b94f5b0bed
Ray-Xavier-2021 <horatioxhanley@gmail.com>
2024-02-01 11:23:34 -0500

    Update index.js output loop

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const MongoClient = require('mongodb').MongoClient;
 // Import password
 const password = require('./password.js');
 const PASSWORD = password.getPassword();
-const CONNECTION_STRING = `mongodb+srv://quotes:${PASSWORD}@quotes-cluster.2cczmmj.mongodb.net/?retryWrites=true&w=majority}`;
+const CONNECTION_STRING = `mongodb+srv://quotes:${PASSWORD}@quotes-cluster.2cczmmj.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create Database Client Connection 
 MongoClient.connect(CONNECTION_STRING)
